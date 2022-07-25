d5ca5911a199d796f09ea5b06444b4faa4d53fc3
Yussef Mrt <52961281+TheXtn@users.noreply.github.com>
2022-07-25 14:07:14 +0100

    up

--- pages/api/[roomID].js
+++ pages/api/[roomID].js
@@ -1,61 +1,8 @@
 import { Server } from 'socket.io'
+import {cards,updateCards} from '../../helper/tekdheb'
 let currentPlayer = null;
 let table = []
 let leaderBoard = []
-let cards = [
-  { id: 1, suit: 'hearts', value: '1', name: 'A' },
-  { id: 2, suit: 'hearts', value: '2', name: '2' },
-  { id: 3, suit: 'hearts', value: '3', name: '3' },
-  { id: 4, suit: 'hearts', value: '4', name: '4' },
-  { id: 5, suit: 'hearts', value: '5', name: '5' },
-  { id: 6, suit: 'hearts', value: '6', name: '6' },
-  { id: 7, suit: 'hearts', value: '7', name: '7' },
-  { id: 8, suit: 'hearts', value: '8', name: '8' },
-  { id: 9, suit: 'hearts', value: '9', name: '9' },
-  { id: 10, suit: 'hearts', value: '10', name: '10' },
-  { id: 11, suit: 'hearts', value: '11', name: 'J' },
-  { id: 12, suit: 'hearts', value: '12', name: 'Q' },
-  { id: 13, suit: 'hearts', value: '13', name: 'K' },
-  { id: 14, suit: 'spades', value: '1', name: 'A' },
-  { id: 15, suit: 'spades', value: '2', name: '2' },
-  { id: 16, suit: 'spades', value: '3', name: '3' },
-  { id: 17, suit: 'spades', value: '4', name: '4' },
-  { id: 18, suit: 'spades', value: '5', name: '5' },
-  { id: 19, suit: 'spades', value: '6', name: '6' },
-  { id: 20, suit: 'spades', value: '7', name: '7' },
-  { id: 21, suit: 'spades', value: '8', name: '8' },
-  { id: 22, suit: 'spades', value: '9', name: '9' },
-  { id: 23, suit: 'spades', value: '10', name: '10' },
-  { id: 24, suit: 'spades', value: '11', name: 'J' },
-  { id: 25, suit: 'spades', value: '12', name: 'Q' },
-  { id: 26, suit: 'spades', value: '13', name: 'K' },
-  { id: 27, suit: 'clubs', value: '1', name: 'A' },
-  { id: 28, suit: 'clubs', value: '2', name: '2' },
-  { id: 29, suit: 'clubs', value: '3', name: '3' },
-  { id: 30, suit: 'clubs', value: '4', name: '4' },
-  { id: 31, suit: 'clubs', value: '5', name: '5' },
-  { id: 32, suit: 'clubs', value: '6', name: '6' },
-  { id: 33, suit: 'clubs', value: '7', name: '7' },
-  { id: 34, suit: 'clubs', value: '8', name: '8' },
-  { id: 35, suit: 'clubs', value: '9', name: '9' },
-  { id: 36, suit: 'clubs', value: '10', name: '10' },
-  { id: 37, suit: 'clubs', value: '11', name: 'J' },
-  { id: 38, suit: 'clubs', value: '12', name: 'Q' },
-  { id: 39, suit: 'clubs', value: '13', name: 'K' },
-  { id: 40, suit: 'diamonds', value: '1', name: 'A' },
-  { id: 41, suit: 'diamonds', value: '2', name: '2' },
-  { id: 42, suit: 'diamonds', value: '3', name: '3' },
-  { id: 43, suit: 'diamonds', value: '4', name: '4' },
-  { id: 44, suit: 'diamonds', value: '5', name: '5' },
-  { id: 45, suit: 'diamonds', value: '6', name: '6' },
-  { id: 46, suit: 'diamonds', value: '7', name: '7' },
-  { id: 47, suit: 'diamonds', value: '8', name: '8' },
-  { id: 48, suit: 'diamonds', value: '9', name: '9' },
-  { id: 49, suit: 'diamonds', value: '10', name: '10' },
-  { id: 50, suit: 'diamonds', value: '11', name: 'J' },
-  { id: 51, suit: 'diamonds', value: '12', name: 'Q' },
-  { id: 52, suit: 'diamonds', value: '13', name: 'K' },
-];
 var connectedPlayers = []
 function getRandomCard() {
   const randomCard = cards[Math.floor(Math.random() * cards.length)];
@@ -71,7 +18,7 @@ function getCards(players) {
       }
     });
   }
-  cards = [
+  updateCards( [
     { id: 1, suit: 'hearts', value: '1', name: 'A' },
     { id: 2, suit: 'hearts', value: '2', name: '2' },
     { id: 3, suit: 'hearts', value: '3', name: '3' },
@@ -124,7 +71,7 @@ function getCards(players) {
     { id: 50, suit: 'diamonds', value: '11', name: 'J' },
     { id: 51, suit: 'diamonds', value: '12', name: 'Q' },
     { id: 52, suit: 'diamonds', value: '13', name: 'K' },
-  ];
+  ]);
 }
 
 function sortCards(players) {
