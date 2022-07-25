1837f9da262b4a458fae44e748f530b26e9a3c63
Islem Bargaoui <slash4games.yt@gmail.com>
2022-07-25 17:14:36 +0100

    update

--- components/Table.js
+++ components/Table.js
@@ -20,7 +20,7 @@ export default function Table(props){
        
         {table.map((item)=>{
             return(
-                <p key={item.player_name} style={{display:'flex'}}>{item.player_name} played :    {item.as!=11 && item.as!=12 && item.as!=13 ? item.as:<p></p>} {item.as==11 ? <p>J</p>:<p></p>} {item.as==12 ? <p>Q</p>:<p></p>} {item.as==13 ? <p>K</p>:<p></p>}</p>
+                <p key={item.player_name} style={{display:'flex'}}>{item.player_name} played :    {item.as!=11 && item.as!=12 && item.as!=13 && item.as!=1 ? item.as:<p></p>} {item.as==1 ? <p>A</p>:<p></p>} {item.as==11 ? <p>J</p>:<p></p>} {item.as==12 ? <p>Q</p>:<p></p>} {item.as==13 ? <p>K</p>:<p></p>}</p>
             )
         })}
         </div>
